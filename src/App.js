5097467b2e9a10275c10231c7747e1c84198b653
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2022-06-14 23:28:51 +0900

    Edit LP as Nezulatoon

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nezulatoon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ushinohi.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ushinohi Project with S
         </a>
       </header>
     </div>
