25cfb045c4c846c0dd12c09ab7a0cf4a5a7ce198
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2022-06-17 01:36:37 +0900

    Add geolocated

--- src/components/molecules/Top.js
+++ src/components/molecules/Top.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Top.css';
 import ApiFetch from './ApiFetch';
+import Map from './Map';
 
 function Top() {
   return (
     <div className="App">
       <header className="App-header">
+        <Map />
         <ApiFetch />
         <a href="/" className="btn btn--orange">Shoot</a>
       </header>
