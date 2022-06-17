b8db34bcc2045b0fb6b4d6d18f1f6b1d6652c1c7
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2022-06-18 06:42:05 +0900

    Devide components per teams

--- src/components/molecules/Top.js
+++ src/components/molecules/Top.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { useGeolocated } from "react-geolocated";
+// import { useGeolocated } from "react-geolocated";
+import { Link } from "react-router-dom";
 import './Top.css';
-import ApiFetch from './ApiFetch';
+// import ApiFetch from './ApiFetch';
 // import Map from './Map';
 
 function Top() {
 
-  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-  useGeolocated({
-      positionOptions: {
-          enableHighAccuracy: false,
-      },
-      userDecisionTimeout: 5000,
-  });
+  // const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+  // useGeolocated({
+  //     positionOptions: {
+  //         enableHighAccuracy: false,
+  //     },
+  //     userDecisionTimeout: 5000,
+  // });
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <Map isGeolocationAvailable={isGeolocationAvailable} isGeolocationEnabled={isGeolocationEnabled} coords={coords} /> */}
-        <ApiFetch isGeolocationAvailable={isGeolocationAvailable} isGeolocationEnabled={isGeolocationEnabled} coords={coords} />
-        <a href="/" className="btn btn--orange">Shoot</a>
+        {/* <ApiFetch isGeolocationAvailable={isGeolocationAvailable} isGeolocationEnabled={isGeolocationEnabled} coords={coords} />
+        <a href="/" className="btn btn--orange">Shoot</a> */}
+        <h2>Select Your Team</h2>
+        <Link className="team-name" to={`/YaegakiShaonkai/`}>八重垣謝恩会 &#8250;</Link>
+        <Link className="team-name" to={`/NezuGinza/`}>根津銀座通り商睦会 &#8250;</Link>
+        <Link className="team-name" to={`/NezuMiyanaga/`}>根津宮永商盛会 &#8250;</Link>
       </header>
     </div>
   );
